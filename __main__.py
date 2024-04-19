c6590e03720f03d2fdec80a1fc718ef79e88f291
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-04-19 16:36:14 +0630

    update(): sg config added

--- __main__.py
+++ __main__.py
@@ -9,7 +9,15 @@ bucket = s3.Bucket('my-bucket')
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
 
+sg = ec2.SecurityGroup("web-server-sg",description="Security group for web servers.")
+
+allow_ssh =ec2.SecurityGroupRule("AllowSSH",type="ingress",from_port=22,to_port=22,protocol="tcp",cidr_blocks=["0.0.0.0/0"],security_group_id=sg.id)
+
+allow_http =ec2.SecurityGroupRule("AllowHTTP",type="ingress",from_port=80,to_port=80,protocol="tcp",cidr_blocks=["0.0.0.0/0"],security_group_id=sg.id)
+
+allow_all =ec2.SecurityGroupRule("AllowALL",type="egress",from_port=0,to_port=0,protocol="tcp",cidr_blocks=["0.0.0.0/0"],security_group_id=sg.id)
+
 ec2_instance = ec2.Instance("web-server",ami="ami-04f73ca9a4310089f",instance_type="t3.nano",key_name="generals-login",tags={
     "Name":"web server"
-})
+},vpc_security_group_ids=[sg.id])
 pulumi.export('public_ip', ec2_instance.public_ip)
